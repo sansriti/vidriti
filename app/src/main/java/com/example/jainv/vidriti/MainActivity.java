52b7e5a9a6ebbac859a24d7011a898cb856e2177
VidushiJain30 <jainvidushi30@gmail.com>
2018-08-31 23:56:26 +0530

    Fragments added

--- app/src/main/java/com/example/jainv/vidriti/MainActivity.java
+++ app/src/main/java/com/example/jainv/vidriti/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.jainv.vidriti;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    FragmentTransaction fragmentTransaction;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +23,60 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.main_container,new HomeFragment());
+        fragmentTransaction.commit();
+        getSupportActionBar().setTitle("Home Fragment");
+        navigationView=(NavigationView)findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container,new HomeFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Home Fragment");
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.setting:
+                        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container,new SettingFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Setting Fragment");
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.wishlist:
+                        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container,new WishlistFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Wishlist Fragment");
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.account:
+                        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container,new AccountFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Account Fragment");
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.order:
+                        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container,new Your_OrderFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Your_Order Fragment");
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                        }
+
+                return true;
+            }
+        });
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
--- app/src/main/java/com/example/jainv/vidriti/SettingFragment.java
+++ app/src/main/java/com/example/jainv/vidriti/SettingFragment.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class SettingFragment extends Fragment {
 
 
-    public HomeFragment() {
+    public SettingFragment() {
         // Required empty public constructor
     }
 
@@ -23,7 +23,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_setting, container, false);
     }
 
 }
